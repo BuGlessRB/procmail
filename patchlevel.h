d5090e173e27d51051ad998e75f445588e6a0982
Stephen R. van den Berg <srb@cuci.nl>
1992-11-13 13:59:16 +0000

    _

--- patchlevel.h
+++ patchlevel.h
@@ -4,7 +4,7 @@ procmail v2.80pre3 1992/11/13 written and created by Stephen R. van den Berg\n\
 \t\t\t\tberg@physik.tu-muenchen.de\n\
 \n\
 Submit questions/answers to the procmail-related mailinglist by sending to:\n\
-\tprocmail@pool.informatik.rwth-aachen.de\n\
+\tprocmail@informatik.rwth-aachen.de\n\
 \n\
 And of course, subscription and information requests for this list to:\n\
-\tprocmail-request@pool.informatik.rwth-aachen.de\n"
+\tprocmail-request@informatik.rwth-aachen.de\n"
