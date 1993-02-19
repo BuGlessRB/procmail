6af0c8ebd9a91e189c4dc5c09ceb5b13335a13c4
Stephen R. van den Berg <srb@cuci.nl>
1993-02-19 14:12:29 +0000

    _

--- patchlevel.h
+++ patchlevel.h
@@ -1,5 +1,5 @@
 #define VERSION "\
-procmail v2.81pre1 1993/02/18 written and created by Stephen R. van den Berg\n\
+procmail v2.81 1993/02/19 written and created by Stephen R. van den Berg\n\
 \t\t\t\tberg@pool.informatik.rwth-aachen.de\n\
 \t\t\t\tberg@physik.tu-muenchen.de\n\
 \n\
