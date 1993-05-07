ab04ea3de3794234161109973fbbfedeb06f496a
Stephen R. van den Berg <srb@cuci.nl>
1993-05-07 11:18:04 +0000

    _

--- src/goodies.h
+++ src/goodies.h
@@ -1,7 +1,7 @@
-/*$Id: goodies.h,v 1.6 1992/12/03 14:15:20 berg Exp $*/
+/*$Id: goodies.h,v 1.7 1993/05/07 13:18:04 berg Exp $*/
 
 void
- readparse P((char*p,int(*const fpgetc)(),const int sarg)),
+ readparse P((char*p,int(*const fpgetc)(),const sarg)),
  sputenv P((const char*const a)),
  primeStdout P((void)),
  retStdout P((char*const newmyenv)),
