fc658c16108372da348efbe95dd8e1efc0741bf9
Stephen R. van den Berg <srb@cuci.nl>
1994-10-28 14:05:56 +0000

    _

--- patchlevel.h
+++ patchlevel.h
@@ -1,5 +1,5 @@
 #define VERSION "\
-procmail v3.10 1994/10/26 written and created by Stephen R. van den Berg\n\
+procmail v3.10 1994/10/28 written and created by Stephen R. van den Berg\n\
 \t\t\t\tberg@pool.informatik.rwth-aachen.de\n\
 \n\
 Submit questions/answers to the procmail-related mailinglist by sending to:\n\
