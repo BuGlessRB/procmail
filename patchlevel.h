7b5c7df585a45b92bc8393e51faa9f3b7ef248cd
Stephen R. van den Berg <srb@cuci.nl>
1998-11-17 23:16:37 +0000

    _

--- patchlevel.h
+++ patchlevel.h
@@ -3,10 +3,10 @@ procmail v3.11pre8 1998/11/04, Copyright (c) 1998, Stephen R. van den Berg\n\
 \t\t\t\t\t\t\t<srb@cuci.nl>\n\
 \n\
 Submit questions/answers to the procmail-related mailinglist by sending to:\n\
-\t<procmail@informatik.rwth-aachen.de>\n\
+\t<procmail-users@procmail.org>\n\
 \n\
 And of course, subscription and information requests for this list to:\n\
-\t<procmail-request@informatik.rwth-aachen.de>\n"
+\t<procmail-users-request@procmail.org>\n"
 
 /* If the formatting or number of newlines of VERSION substantially changes,
    src/manconf.c needs to be changed as well */
